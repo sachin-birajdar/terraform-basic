fb8cc4c87c5d786efe9614d9c0a7c4bd1c0ee621
sachin <sachinbirajdar0404@gmail.com>
2022-12-10 14:40:18 +0530

    adding securty group

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -29,6 +29,14 @@ resource "aws_security_group" "allow_ssh" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+  }
+    
+  ingress {
+    description      = "HHTP from public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
    
   }
 
