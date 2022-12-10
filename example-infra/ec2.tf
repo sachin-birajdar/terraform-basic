ce526b7f69676c1e39e581c8facc84c41e91e578
sachin <sachinbirajdar0404@gmail.com>
2022-12-10 14:06:55 +0530

    adding securty group

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -4,6 +4,8 @@ ami = "ami-02367f873ab738292"
 
 instance_type =  "t2.micro"
 
+vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
 tags = {
     Name = "terraform instance"
 }
@@ -13,4 +15,32 @@ tags = {
 output "private_ip" {
     value = aws_instance.Web.private_dns
    
+}
+
+#create security group
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
